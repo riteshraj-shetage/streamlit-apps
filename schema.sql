5355dc2fe4cb81ee556211700bb0a36975b2e2e5
Riteshraj Shetage <144100040+riteshraj-shetage@users.noreply.github.com>
2025-12-04 13:40:01 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE simple_auth;
+USE simple_auth;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
