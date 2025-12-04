6741896860108d97da85c4a71ecd3c5bda0c765f
Riteshraj Shetage <144100040+riteshraj-shetage@users.noreply.github.com>
2025-12-04 13:44:42 +0530

    Create home.php

---
+++ ui/home.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+
+<div class="text-center">
+  <h2>Welcome to MyApp!</h2>
+  <p>This is a simple frontend demo with Bootstrap.</p>
+  <a href="login.php" class="btn btn-success mt-3">Login</a>
+  <a href="register.php" class="btn btn-primary mt-3">Register</a>
+</div>
+
+<?php include 'footer.php'; ?>
