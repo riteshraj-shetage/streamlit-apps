0804da56782981caca7df49ab11b0c6aafb3a1dd
Riteshraj Shetage <144100040+riteshraj-shetage@users.noreply.github.com>
2025-12-10 17:06:50 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-    <footer class="bg-dark text-white text-center py-2">
-      <small>&copy; <?php echo date("Y"); ?> MyApp</small>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
